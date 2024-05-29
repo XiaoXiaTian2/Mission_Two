05e084df8cfb96bb6bda8a17ded597da196a6554
XiaoXiaTian2 <s59444997880@gmail.com>
2024-05-29 17:07:48 +0800

    助教建議修正

---
+++ JS/main.js
@@ -0,0 +1,23 @@
+// toggle 漢堡包
+const menuToggle = document.querySelector('.toggle-menu')
+const mobileNavList = document.querySelector('.mobile-menu')
+const body = document.getElementsByTagName('html')[0]
+
+menuToggle.addEventListener('click', function() {
+    mobileNavList.classList.toggle('active')
+    menuToggle.classList.toggle('active')
+    if(menuToggle.classList.length === 1){
+        menuToggle.innerHTML = `<span class="material-symbols-outlined">
+        menu
+        </span>`
+
+    }else{
+        menuToggle.innerHTML = `<span class="material-symbols-outlined">
+        close
+        </span>`
+        // console.log(body)
+        // body.style.zIndex = -1
+        // body.style.backgroundColor = 'blue'
+    }
+})
+
